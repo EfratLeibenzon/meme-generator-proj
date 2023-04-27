e911e82778127d20d36441e0dd93ee6d476e494d
Efrat Leibenzon <efrat.leibenzon@gmail.com>
2023-04-27 19:55:37 +0300

    changed the data model,updated all the functions accordingly, add display none to sections

--- js/controller/meme-editor.controller.js
+++ js/controller/meme-editor.controller.js
@@ -1,12 +1,15 @@
 'use strict'
 let gElCanvas = document.querySelector("#meme-canvas")
 let gCtx = gElCanvas.getContext('2d')
-let g1stLineX = gElCanvas.width / 4
-let g1stLineY = gElCanvas.height / 4
-// let g2ndLineX=gElCanvas.height-gElCanvas.height/4
-// let g2ndLineY=gElCanvas.width-gElCanvas.width/4
+let gLineX = gElCanvas.width / 2
+let gLineY
 
 function onUploadImg(imgId) {
+    let elImgGallery = document.querySelector('.img-gallery')
+    console.log(elImgGallery)
+    elImgGallery.classList.add('hide')
+    let elMemeEditor = document.querySelector('.meme-editor')
+    elMemeEditor.classList.remove('hide')
     setMemeImg(imgId)
     renderImg(imgId)
 
@@ -20,17 +23,28 @@ function renderImg(imgId) {
 function renderMeme() {
     var userInput = document.getElementById("text-input").value
     setLineTxt(userInput)
-    renderImg(getMeme().imgId)
+    renderImg(getMeme().selectedImgId)
+
+    if (getMeme().selectedLineIdx === 0) {
+        gLineY = gElCanvas.height / 4
+
+    } else if (getMeme().selectedLineIdx === 1) {
+        gLineY = gElCanvas.height - gElCanvas.height / 4
+
+    } else {
+        gLineY = gElCanvas.height / 2
+    }
 
     gCtx.lineWidth = 2
-    gCtx.strokeStyle = getMeme().colorStroke
-    gCtx.fillStyle = getMeme().colorFill
-    gCtx.font = getMeme().fontSize + 'px Impact'
-    gCtx.fillText(userInput, g1stLineX, g1stLineY)
-    gCtx.strokeText(userInput, g1stLineX, g1stLineY)
+    gCtx.strokeStyle = getMeme().lines[gMeme.selectedLineIdx].colorStroke
+    gCtx.fillStyle = getMeme().lines[gMeme.selectedLineIdx].colorFill
+    gCtx.font = getMeme().lines[gMeme.selectedLineIdx].size + 'px Impact'
+    gCtx.fillText(userInput, gLineX, gLineY)
+    gCtx.strokeText(userInput, gLineX, gLineY)
 
     console.log('gmeme', gMeme)
     console.log(gCtx.font)
+
 }
 
 function onFontSizeChange(sizeChange) {
@@ -38,8 +52,8 @@ function onFontSizeChange(sizeChange) {
     renderMeme()
 }
 
-function onTxtColorChange(inputvalue) {
-    setTxtColor(inputvalue)
+function onTxtColorChange(inputValue) {
+    setTxtColor(inputValue)
     renderMeme()
 
 }
--- js/services/meme-editor.service.js
+++ js/services/meme-editor.service.js
@@ -1,11 +1,24 @@
-var gMeme = { imgId: '', userInput: ['', ''], fontSize: 40, colorFill: 'white', colorStroke: 'black' }
+'use strict'
+var gMeme = {
+    selectedImgId: 0,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: '',
+            size: 40,
+            align: 'left',
+            colorFill: 'white',
+            colorStroke: 'black',
+        }
+    ]
+}
 
 function setMemeImg(imgId) {
-    gMeme.imgId = imgId
+    gMeme.selectedImgId = imgId
     console.log('gMeme', gMeme)
 }
 function setLineTxt(userInput) {
-    gMeme.userInput = userInput
+    gMeme.lines[gMeme.selectedLineIdx].txt = userInput
     saveToStorage('gMeme', gMeme)
 }
 
@@ -16,20 +29,27 @@ function getMeme() {
 function setLineFontSize(sizeChange) {
     console.log('sizeChange:', sizeChange)
     if (sizeChange === 1) {
-        gMeme.fontSize += 10
+        gMeme.lines[gMeme.selectedLineIdx].size += 10
     }
     else {
-        gMeme.fontSize -= 10
+        gMeme.lines[gMeme.selectedLineIdx].size -= 10
     }
 }
 
 function setTxtColor(inputvalue) {
-    gMeme.colorFill = inputvalue
+    gMeme.lines[gMeme.selectedLineIdx].colorFill = inputvalue
     console.log('gmeme', gMeme)
 }
 
 function AddLine() {
-
+    gMeme.selectedLineIdx++
+    gMeme.lines.push({
+        txt: '',
+        size: 40,
+        align: 'left',
+        colorFill: 'white',
+        colorStroke: 'black'
+    })
 }
 
 function DeleteLine() {
